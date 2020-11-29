ad18a7cde6af86e9b3337afbce2cdc8cb455f9bc
thanhchuc-hcmue-it46 <thanhchucnaalun02@gmail.com>
2020-11-29 23:55:45 +0700

    Bài dùng lệnh if-else

---
+++ cpp/Bai_dung_lenh_if_else.cpp
@@ -0,0 +1,41 @@
+/* Copyright@2020 by Thanh Chuc Le */
+
+// Input:
+// Một số nguyên dương n (n<=10^4)
+// Ouput: Nếu n chia hết cho 3, xuất "n chia het cho 3", ngược lại nếu n chia cho 3 dư 1 xuất "n chia 3 du 1" ngược lại nếu n chia 3 dư 2 xuất "n chia 3 du 2"
+// (Lưu ý: Không xuất dấu "
+// Thay n bằng giá trị input
+// Ví dụ:
+
+// Input	Output
+// 9       9 chia het cho 3
+// 5       5 chia 3 du 2
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Nhập dữ liệu
+    int n;
+    cin>>n;
+    
+    // Tính toán
+    int sodu = n % 3;
+    
+    // Xuất kết quả
+    if (sodu  == 0) {
+        cout<<n<<" chia het cho 3";
+    }
+    else if (sodu == 1) {
+        cout<<n<<" chia 3 du 1";
+    }
+    else if (sodu == 2) {
+        cout<<n<<" chia 3 du 2";
+    }
+    
+    
+    return 0;
+}
