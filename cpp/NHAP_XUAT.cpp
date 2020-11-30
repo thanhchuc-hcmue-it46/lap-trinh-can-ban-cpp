fec2e935140abc056e7d608d8300ba62ce83f6d3
thanhchuc-hcmue-it46 <thanhchucnaalun02@gmail.com>
2020-11-30 21:18:55 +0700

    Bài nhập xuất

---
+++ cpp/NHAP_XUAT.cpp
@@ -0,0 +1,20 @@
+/* Copyright@2020 by Thanh Chuc Le */
+// Yêu cầu xuất ra màn hình các dãy số như sau:
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout<<1<<"\n";
+    cout<<12<<"\n";
+    cout<<123<<"\n";
+    cout<<1234<<"\n";
+    cout<<12345;
+    
+    return 0;
+}
