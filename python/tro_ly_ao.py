0a30a9ffd448c4a2e4b3b8848367d4a5315c175e
thanhchuc-hcmue-it46 <thanhchucnaalun02@gmail.com>
2020-12-05 15:51:47 +0700

    Trợ lý ảo hoàn chỉnh

--- python/tro_ly_ao.py
+++ python/tro_ly_ao.py
@@ -1,29 +1,50 @@
 import speech_recognition as sr
 import pyttsx3
+from datetime import date
+from datetime import datetime
 
-# Robot ear
 robot_ear = sr.Recognizer()
-with sr.Microphone() as mic:
-    print("Robot: I'm Listening")
-    audio = robot_ear.listen(mic)
-
-you = ""
-try:
-    you = robot_ear.recognize_google(audio)
-except:
-    you = ""
-
-print("You: " + you) 
-
-# Robot brain
-robot_brain = ""
-if you == "":
-    robot_brain = "I can't hear you, try again"
-elif you == "hello":
-    robot_brain = "Hello Chuc Xinh"
-
-# Robot mount
-print("Robot: " + robot_brain)
 engine = pyttsx3.init()
-engine.say(robot_brain)
-engine.runAndWait()
+robot_brain = ""
+
+while True:
+    # Robot ear
+    with sr.Microphone() as mic:
+        print("Robot: I'm Listening")
+        audio = robot_ear.listen(mic)
+    print("Robot: ...")
+
+    try:
+        you = robot_ear.recognize_google(audio)
+    except:
+        you = ""
+
+    print("You: " + you) 
+
+    # Robot brain
+    if you == "":
+        robot_brain = "I can't hear you, try again"
+    elif "hello" in you:
+        robot_brain = "Hello Chuc Xinh"
+    elif "today" in you:
+        today = date.today()
+        robot_brain = today.strftime("%B %d, %Y")
+    elif "time" in you:
+        now = datetime.now()
+        robot_brain = now.strftime("%H hours %M minutes %S seconds")
+    elif "president" in you:
+        robot_brain = "Donald Trunp"
+    elif "bye" in you:
+        robot_brain = "Bye Chuc xinh"
+        print("Robot: " + robot_brain)
+        engine.say(robot_brain)
+        engine.runAndWait()
+        break
+    else:
+        robot_brain = "I'm fine thank you anh you"
+
+    # Robot mount
+    print("Robot: " + robot_brain)
+    engine = pyttsx3.init()
+    engine.say(robot_brain)
+    engine.runAndWait()
